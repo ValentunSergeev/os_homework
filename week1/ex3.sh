bd5188561f5b85d4d1e3d51013d5eb5149e7e3ea
Valentun <karbon4ic@gmail.com>
2019-08-21 16:07:56 +0300

    Week 1

---
+++ week1/ex3.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo $(date +%F)
+
+sleep 3
+
+echo $(date +%F)
---
+++ week1/main.c
@@ -0,0 +1,10 @@
+//
+// Created by valentun on 21.08.19.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello world");
+    return 0;
+}
